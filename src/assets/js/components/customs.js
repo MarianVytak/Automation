55edc04dbadcf9b1f22ee8fbefa3b9751485f775
MarianVytak <marianvytak@gmail.com>
2020-03-28 18:35:03 +0200

    Added Ventilation Main.

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -2,4 +2,16 @@ $(function () {
 
     'use strict';
 
+    let viewAll = $('.view-all');
+
+    viewAll.on('click', function (e) {
+        e.preventDefault();
+        let viewAllText = $(this).text();
+        if (viewAllText === 'Развернуть') {
+            $(this).text('Свернуть');
+        } else if (viewAllText === 'Свернуть') {
+            $(this).text('Развернуть');
+        }
+    });
+
 });
