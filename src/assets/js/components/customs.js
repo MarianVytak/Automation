87ced4748788637adc2a52ef5b3dc7d1180251d8
MarianVytak <marianvytak@gmail.com>
2020-04-03 01:12:24 +0300

    Ventilation v2.0

--- src/assets/js/components/customs.js
+++ src/assets/js/components/customs.js
@@ -16,12 +16,41 @@ $(function () {
 
 
 
-    // Hamburger
-    let hamburger = $('.hamburger');
-    hamburger.on('click', function(){
-        hamburger.toggleClass('hamburger-active');
-        $('.header__mid').slideToggle();
-        $('.header__bot').slideToggle();
+    // Menu
+    let menuLink = $('.js-menu-link'),
+        menuClose = $('.menu-close'),
+        overlay = $('.overlay');
+    menuLink.on('click', function (e) {
+        e.preventDefault();
+        let thisMenuLink = $(this).attr('data-target');
+        $(thisMenuLink).addClass('active');
+        overlay.addClass('active');
+        $('html, body').addClass('hidden');
+    });
+    menuClose.on('click', function (e) {
+        e.preventDefault();
+        let thisMenuClose = $(this).attr('data-target');
+        $(thisMenuClose).removeClass('active');
+        overlay.removeClass('active');
+        $('html, body').removeClass('hidden');
+    });
+    overlay.on('click', function (e) {
+        e.preventDefault();
+        $('.js-menu').removeClass('active');
+        overlay.removeClass('active');
+        $('html, body').removeClass('hidden');
+    });
+
+
+
+    // Mobile Search
+    $('.header__bot_mobile_search_icon').on('click', function (e) {
+        e.preventDefault();
+        $('.header__bot_mobile').addClass('active');
+    });
+    $('.header__bot_mobile_search_close').on('click', function (e) {
+        e.preventDefault();
+        $('.header__bot_mobile').removeClass('active');
     });
 
 });
